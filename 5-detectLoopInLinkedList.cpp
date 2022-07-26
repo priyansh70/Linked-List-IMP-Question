7c8fb08c425f2423c1ca5d3b491abee3ad539c9d
Priyansh Patel <93965871+priyansh70@users.noreply.github.com>
2022-07-26 13:47:04 +0530

    Create 5-detectLoopInLinkedList.cpp

---
+++ 5-detectLoopInLinkedList.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include<map>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertion(Node *&tail, int element, int data)
+{
+    // if Linked List is Empty
+    if (tail == NULL)
+    {
+        Node *temp = new Node(data);
+        temp->next = temp;
+        tail = temp;
+    }
+
+    else
+    {
+
+        Node *current = tail->next;
+
+        while (current->data != element)
+        {
+            current = current->next;
+        }
+
+        if (current->data == element)
+        {
+            Node *temp = new Node(data);
+            temp->next = current->next;
+            current->next = temp;
+        }
+    }
+}
+
+void printCLL(Node* tail){
+    Node* temp = tail;
+    do{
+        cout<<tail->data<<" ";
+        tail = tail->next;
+    }while(tail != temp);
+    cout<<endl;
+}
+
+// Detect LOOP
+bool detectLoop(Node* head)
+{
+    if(head == NULL || head -> next == NULL)
+    {
+        return false;
+    }
+
+    Node* fast = head;
+    Node* slow = head;
+
+    while(fast&& fast-> next)
+    {
+        slow = slow -> next;
+        fast = fast -> next -> next;
+
+        if(slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    Node *tail = NULL;
+    
+    insertion(tail, 4, 10);
+    printCLL(tail);
+    
+    insertion(tail, 10, 4);
+    printCLL(tail);
+    
+    insertion(tail, 4, 12);
+    printCLL(tail);
+    
+    insertion(tail, 10, 64);
+    printCLL(tail);
+    
+    insertion(tail, 64, 1);
+    printCLL(tail);
+    
+    insertion(tail, 12, 48);
+    printCLL(tail);
+
+    if(detectLoop(tail))
+    {
+        cout<<"Cycle is present in List"<<endl;
+    }
+    else
+    {
+        cout<<"Cycle is not present in List"<<endl;
+    }
+
+    return 0;
+}
